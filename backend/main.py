fdc8eba632be512a99fcab87c5d871320c17861b
Arslan <arslan.scheema1997@gmail.com>
2025-08-14 13:00:54 +0500

    added sqlachemy 2.0.43 & port 10000

--- backend/main.py
+++ backend/main.py
@@ -63,4 +63,4 @@ async def health_check():
 
 if __name__ == "__main__":
  
-    uvicorn.run(app, host="0.0.0.0", port=os.environ("PORT")) 
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
